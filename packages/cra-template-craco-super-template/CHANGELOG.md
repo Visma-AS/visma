98557eaf148600311daf17cea8c5f56675603b8d
Arno Saine <arno@mowhi.com>
2021-09-24 20:40:03 +0300

    @visma/cra-template-craco-super-template@0.1.2
     - Remove extraneous `<IntlProvider>`
     - Set corrent default language in `.gitignore`
     - Fix incorret use of `useIntl` in the component tree
     - Update example data

--- packages/cra-template-craco-super-template/CHANGELOG.md
+++ packages/cra-template-craco-super-template/CHANGELOG.md
@@ -1,3 +1,10 @@
+# 0.1.2
+
+- Remove extraneous `<IntlProvider>`
+- Set corrent default language in `.gitignore`
+- Fix incorret use of `useIntl` in the component tree
+- Update example data
+
 # 0.1.1
 
 - Fix incorrect template dependencies
--- packages/cra-template-craco-super-template/template/src/components/App.js
+++ packages/cra-template-craco-super-template/template/src/components/App.js
@@ -1,40 +1,48 @@
 import AppProvider from '@visma/react-app-super-template';
-import { IntlProvider } from '@visma/react-intl-bundled-messages';
 import { FormattedMessage, useIntl } from 'react-intl';
 import logo from '../logo.svg';
 import './App.css';
 import Info from './Info';
 
-function App() {
-  const intl = useIntl();
-
+export default function App() {
   return (
     <AppProvider fallback={null}>
-      <IntlProvider>
-        <div className="App">
-          <header className="App-header">
-            <img
-              src={logo}
-              className="App-logo"
-              alt={intl.formatMessage({ defaultMessage: 'logo' })}
+      <div className="App">
+        <header className="App-header">
+          <Image />
+          <p>
+            <FormattedMessage
+              defaultMessage="Edit <code>src/App.js</code> and save to reload."
+              values={{
+                code: (chunks) => <code>{chunks}</code>,
+              }}
             />
-            <p>
-              <FormattedMessage defaultMessage="Edit <code>src/App.js</code> and save to reload." />
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FormattedMessage defaultMessage="Learn React" />
-            </a>
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FormattedMessage defaultMessage="Learn React" />
+          </a>
+          <p>
             <Info />
-          </header>
-        </div>
-      </IntlProvider>
+          </p>
+        </header>
+      </div>
     </AppProvider>
   );
 }
 
-export default App;
+function Image() {
+  const intl = useIntl();
+
+  return (
+    <img
+      src={logo}
+      className="App-logo"
+      alt={intl.formatMessage({ defaultMessage: 'logo' })}
+    />
+  );
+}

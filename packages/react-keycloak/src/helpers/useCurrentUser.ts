48b29b7018e3b0d824dd916f39285a0543889976
Arno Saine <arno@mowhi.com>
2021-11-15 18:43:24 +0200

    Fix casing

--- packages/react-keycloak/src/helpers/useCurrentUser.ts
+++ packages/react-keycloak/src/helpers/useCurrentUser.ts
@@ -1,6 +1,6 @@
 import { useKeycloak } from '@react-keycloak/web';
 
-export default function useCurrentuser() {
+export default function useCurrentUser() {
   const { keycloak } = useKeycloak();
   return keycloak.idTokenParsed;
 }
